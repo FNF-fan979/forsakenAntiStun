de5f0fafc1652ece05e3f6fb6dc32f9582059c54
FNF-fan979 <miles.hooper18@gmail.com>
2026-01-04 15:56:00 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+local times = 0
+
 local thing = game:GetService("RunService").RenderStepped:Connect(function()
     game.Players.LocalPlayer.Character.Humanoid:SetAttribute("BaseSpeed", 100)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
@@ -5,9 +7,15 @@ local thing = game:GetService("RunService").RenderStepped:Connect(function()
 end)
 
 game.Players.LocalPlayer.Character.Humanoid.Died:Connect(function()
-    thing:Disconnect()
+    times += 1
+    if times = 2 then
+        thing:Disconnect()
+    end
 end)
 
 game.Players.LocalPlayer.Character.Humanoid.Destroying:Connect(function()
-    thing:Disconnect()
+    times += 1
+    if times = 2 then
+        thing:Disconnect()
+    end
 end)
